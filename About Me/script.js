24173fd314b1fff166e2ccb6222c1984f1b51c52
Joel Montano <joel97montano@gmail.com>
2022-07-08 20:00:25 -0700

    final syntax related stuff - deleting and creating react app in next part

--- About Me/script.js
+++ About Me/script.js
@@ -1,34 +1,57 @@
-const page =(
-    <div>
-        <h1>Hello, I'm Joel</h1>
-        <img src="antonio-minion.jpeg" alt="casa de mini boss" width="90px"></img>
+//import React from "react"
+//import ReactDOM from "react-dom"
+//import Header from "./Header"
+//import PageBody from "./PageBody"
+// import Footer from "./Footer"
+function Header(){
+    return(
+        <header>
+            <nav className="nav">
+            <img src="antonio-minion.jpeg" alt="casa de mini boss"></img>
+            <ul className="nav-items">
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact Me</li>
+                </ul>
+            </nav>
+            <h1 className="title">Hello, I'm Joel</h1>
+        </header>
+    )
+}
+function Footer(){
+    return(
+        <footer>
+           <small>© 2022 Joel's Personal Site.</small> 
+        </footer>
+    )
+}
+function PageBody(){
+    return(
+        <div>
         <h2>I'm learning how to design & build software that doesn't suck</h2>
         <ul>
             <li>I am a PM at Microsoft</li>
             <li>Focusing on Full-Stack development (for now)</li>
             <li>Have experience in Growth and Product</li>
-            <li>Excited about EdTech, HealthTech, marketplace, and consumer businesses that unburden people's lives.</li>
+            <li>Excited about marketplace, consumer and software businesses that unburden people's lives.</li>
             <li>I'm giving myself 3 years (2025) to apply to grad school, walk-on to the football team, and win a conference championship 🫡</li>
         </ul>
-    </div>
-)
-
-function MainContent(){
-    return(
-        page
+        </div>
     )
 }
 
+function PageContent(){
+    return(
+        <div>
+            <Header />
+            <PageBody />
+            <Footer />
+         </div>
+)
+}
 
-// Challenge: Create your own custom React component!
-// Call it "MainContent", and have it return a simple
-// h1 element that says "I'm learning React!"
-
-// Afterward, render it below the Navbar (which
-// you can do inside the ReactDOM.render call below)
 ReactDOM.render(
-    <div>
-        <MainContent />
-    </div>,
+        <PageContent />
+    ,
     document.getElementById("root")
 ) 
